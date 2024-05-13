9166777e3ed864be4ff91a9c0d2acc0be1d02986
Dhieu <112936899+Dhie-boop@users.noreply.github.com>
2024-05-13 11:00:23 +0000

    changes on the about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I'm a Back-End Developer | Microsoft Learn Student Ambassador - Bugema University | AI & ML Enthusiast.";
 
 const About = () => {
   return (
