1cece27c30f8408c732dccf996b7f87b7c2d2a0c
Dhieu <112936899+Dhie-boop@users.noreply.github.com>
2024-05-13 11:17:59 +0000

    some slide changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Back-end-developer-portfolio";
+import image from "../images/Back-end-developer-portfolio.jpg";
 
 const imageAltText = "Image of computer and someone typing";
 
